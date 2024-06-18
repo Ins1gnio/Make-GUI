3da1b91ac159491e38d91184ae7c05636595d708
Ins1gnio <121915989+Ins1gnio@users.noreply.github.com>
2024-06-18 20:31:54 +0700

    Update Practice 1.py

--- QT/Practice 1.py
+++ QT/Practice 1.py
@@ -1,12 +1,11 @@
-# Practice 1 - Generate window, changing size and position
-
+# Practice 1 - generate window, changing size and position
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget
 
 
-class MyApp(QWidget):
-    def __init__(self):
-        super().__init__()
+class MyApp(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.setwindow()
 
     def setwindow(self):
@@ -14,16 +13,14 @@ class MyApp(QWidget):
         self.setGeometry(0, 0, 500, 350)    # define position and window size
         # self.resize(500, 350)   # change window size only
 
-        # re-position window in the middle
         q_rect = self.frameGeometry()   # window geometry
         cw = QDesktopWidget().availableGeometry().center()  # determine center of the screen loc.
         q_rect.moveCenter(cw)
-        self.move(q_rect.topLeft())
-
-        self.show()
+        self.move(q_rect.topLeft())     # move window to the middle
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyApp()
+    ex.show()
     sys.exit(app.exec_())
