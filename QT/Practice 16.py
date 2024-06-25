225fcc2153c3ec946f193fcd608b385291378eae
Ins1gnio <121915989+Ins1gnio@users.noreply.github.com>
2024-06-25 13:26:42 +0700

    Add files via upload

---
+++ QT/Practice 16.py
@@ -0,0 +1,42 @@
+# Practice 16 - Exit button
+
+import sys
+from PyQt5.QtWidgets import QApplication, QDesktopWidget, QMainWindow, QWidget, QPushButton, QMessageBox
+
+
+class MyApp(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setwindow()
+
+        self.button_1 = QPushButton('Exit', self)  # define exit button
+        self.button_1.setShortcut('Escape')
+
+        self.central_widget = QWidget(self)
+        self.setCentralWidget(self.button_1)  # place the widget as a whole window
+
+        self.button_1.clicked.connect(self.exit)
+
+    def exit(self):
+        mbox = QMessageBox.question(self, 'Exit', 'Really want to exit?', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if mbox == QMessageBox.Yes:
+            self.close()
+        elif mbox == QMessageBox.No:
+            pass    # do nothing
+
+    def setwindow(self):
+        self.setWindowTitle("QT-Practice 16")
+        self.setGeometry(0, 0, 200, 100)  # define position and window size
+
+        q_rect = self.frameGeometry()  # window geometry
+        cw = QDesktopWidget().availableGeometry().center()  # determine center of the screen loc.
+        q_rect.moveCenter(cw)
+
+        self.move(q_rect.topLeft())  # move window to the middle
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    ex.show()
+    sys.exit(app.exec_())
